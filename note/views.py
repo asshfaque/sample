afe721126dd16eea9481a4eace3a887628858b2d
asshfaque <you@example.com>
2025-12-01 22:22:30 +0530

    first

--- note/views.py
+++ note/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def intro(request):
-    return render(request,"intro loading page.html")
+    return render(request,"index.html")
 
 def home(request):
     return render(request,"home.html")
